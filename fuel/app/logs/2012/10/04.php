1f3b8113aa6ef38d68ea4c6fde63fe39c7f2d59e
Tim Smith <tsmith@bfgcom.com>
2012-10-04 16:05:21 -0400

    boxfile update

---
+++ fuel/app/logs/2012/10/04.php
@@ -0,0 +1,6 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Warning - 2012-10-04 19:09:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-10-04 19:09:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-10-04 19:09:19 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-10-04 19:09:20 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
